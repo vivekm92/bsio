19456a7ca9922a60ccbd49d47d024a12d0e97ba2
vivek_mittal <vivek.mittal@whitehatjr.com>
2021-01-02 18:27:03 +0530

    Add p904

---
+++ src/p904.py
@@ -0,0 +1,22 @@
+
+class Solution:
+
+	# Binary Seach : T(n) : O(nlogn), S{n} : O(1)
+    def solve(self, piles, k):
+
+        def check(piles, t): 
+            cnt = 0
+            for i in range(len(piles)):
+                cnt += ceil(piles[i] / t)
+            return cnt
+
+        l, r = 1, 100000
+        while l < r:
+            mid = l + (r - l) // 2
+
+            if check(piles, mid) <= k :
+                r = mid
+            else:
+                l = mid + 1
+
+        return l
